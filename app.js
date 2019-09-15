0b95a446d5df1ec50174e9b9d84b11f135ac6de1
Donnie <donald.batryn@gmail.com>
2019-09-15 01:01:30 -0500

    half way mern twitter

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+// const User = require("./models/User");
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World??"));
+
+
+// app.get("/", (req, res) => {
+//     // const user = new User({
+//     //     handle: "Donnie",
+//     //     email: "donnieb@gmail.com",
+//     //     password: "1234567"
+//     // })
+//     // user.save();
+//     res.send("Express App working");
+// });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
